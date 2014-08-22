7cddfe926aa25a762ad421943a0ff416c09bfbdc
Pawel Janeczek <pawel.janeczek@polidea.pl>
2014-08-22 13:40:21 +0200

    First commit

---
+++ BuyPolish/BuyPolish/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BuyPolish
+//
+//  Created by Pawel Janeczek on 22.08.2014.
+//  Copyright (c) 2014 PJMS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([BPAppDelegate class]));
+	}
+}
