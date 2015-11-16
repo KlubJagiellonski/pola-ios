1bc155bd6c64209009f19fae0ffb82a2c2ca266c
Paweł Janeczek <pjanecze@gmail.com>
2015-11-16 23:47:08 +0100

    Urls opened from about web views, opens safari

--- BuyPolish/Pola/UI/ProductSearch/About/BPAboutWebViewController.h
+++ BuyPolish/Pola/UI/ProductSearch/About/BPAboutWebViewController.h
@@ -6,7 +6,7 @@
 #import <Foundation/Foundation.h>
 
 
-@interface BPAboutWebViewController : UIViewController
+@interface BPAboutWebViewController : UIViewController <UIWebViewDelegate>
 
 @property (nonatomic, readonly) NSString *url;
 
--- BuyPolish/Pola/UI/ProductSearch/About/BPAboutWebViewController.m
+++ BuyPolish/Pola/UI/ProductSearch/About/BPAboutWebViewController.m
@@ -25,7 +25,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.castView loadRequest:[NSURLRequest requestWithURL:[[NSURL alloc] initWithString:self.url]]];
+    self.castView.delegate = self;
 }
+
+#pragma mark - UIWebViewDelegate
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    if(navigationType == UIWebViewNavigationTypeOther) {
+        return YES;
+    }
+
+    [[UIApplication sharedApplication] openURL:request.URL];
+    return NO;
+}
+
+#pragma mark - Helpers
 
 - (UIWebView *)castView {
     return (UIWebView *) self.view;
