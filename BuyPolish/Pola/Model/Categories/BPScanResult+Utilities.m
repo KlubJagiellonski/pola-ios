7033704274139dcb46bc3a03f66d4f15de5cfb61
Arek Banas <ext-arkadiusz.banas@here.com>
2016-02-14 13:13:55 +0100

    handleNull method replaced with nilOrStringForKey/nilOrNumberForKey methods in BPScanReslult+Utilities.m

--- BuyPolish/Pola/Model/Categories/BPScanResult+Utilities.m
+++ BuyPolish/Pola/Model/Categories/BPScanResult+Utilities.m
@@ -1,24 +1,24 @@
 #import "BPScanResult+Utilities.h"
-
+#import "NSDictionary+Parsing.h"
 
 @implementation BPScanResult (Utilities)
 
 - (void)parse:(NSDictionary *)dictionary {
-    self.productId = [BPUtilities handleNull:dictionary[@"product_id"]];
-    self.code = [BPUtilities handleNull:dictionary[@"code"]];
-    self.name = [BPUtilities handleNull:dictionary[@"name"]];
-    self.cardType = [self parseCardType:[BPUtilities handleNull:dictionary[@"card_type"]]];
-    self.plScore = [BPUtilities handleNull:dictionary[@"plScore"]];
-    self.altText = [BPUtilities handleNull:dictionary[@"altText"]];
-    self.plCapital = [BPUtilities handleNull:dictionary[@"plCapital"]];
-    self.plWorkers = [BPUtilities handleNull:dictionary[@"plWorkers"]];
-    self.plRnD = [BPUtilities handleNull:dictionary[@"plRnD"]];
-    self.plRegistered = [BPUtilities handleNull:dictionary[@"plRegistered"]];
-    self.plNotGlobEnt = [BPUtilities handleNull:dictionary[@"plNotGlobEnt"]];
-    self.descr = [BPUtilities handleNull:dictionary[@"description"]];
-    self.reportText = [BPUtilities handleNull:dictionary[@"report_text"]];
-    self.reportButtonText = [BPUtilities handleNull:dictionary[@"report_button_text"]];
-    self.reportButtonType = [self parseReportButtonType:[BPUtilities handleNull:dictionary[@"report_button_type"]]];
+    self.productId = [dictionary nilOrNumberForKey:@"product_id"];
+    self.code = [dictionary nilOrStringForKey:@"code"];
+    self.name = [dictionary nilOrStringForKey:@"name"];
+    self.cardType = [self parseCardType:[dictionary nilOrStringForKey:@"card_type"]];
+    self.plScore = [dictionary nilOrNumberForKey:@"plScore"];
+    self.altText = [dictionary nilOrStringForKey:@"altText"];
+    self.plCapital = [dictionary nilOrNumberForKey:@"plCapital"];
+    self.plWorkers = [dictionary nilOrNumberForKey:@"plWorkers"];
+    self.plRnD = [dictionary nilOrNumberForKey:@"plRnD"];
+    self.plRegistered = [dictionary nilOrNumberForKey:@"plRegistered"];
+    self.plNotGlobEnt = [dictionary nilOrNumberForKey:@"plNotGlobEnt"];
+    self.descr = [dictionary nilOrStringForKey:@"description"];
+    self.reportText = [dictionary nilOrStringForKey:@"report_text"];
+    self.reportButtonText = [dictionary nilOrStringForKey:@"report_button_text"];
+    self.reportButtonType = [self parseReportButtonType:[dictionary nilOrStringForKey:@"report_button_type"]];
 }
 
 - (CardType)parseCardType:(NSString *)cardType {
