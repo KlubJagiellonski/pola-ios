a42d241e3c64d0093f9606abacaa4612a8d6917a
Paweł Janeczek <pawel.janeczek@polidea.com>
2016-05-16 12:02:34 +0200

    Initial commit

---
+++ Showroom/Showroom/AppDelegate.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.backgroundColor = UIColor.whiteColor()
+        window?.rootViewController = RootViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
