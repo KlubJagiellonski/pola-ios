9d7be5093f5124f906e6e2687ab99463d27e2dc2
Paweł Janeczek <pawel.janeczek@polidea.com>
2016-05-17 11:08:06 +0200

    Added quick and nimble for testing. Remove Api target.

--- Showroom/ShowroomApi/ShowroomApi.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public class ShowroomApi {
-    public init (){
-        print("Created showroom api")
-    }
-}
--- Showroom/ShowroomTests/ShowroomTests.swift
+++ Showroom/ShowroomTests/ShowroomTests.swift
@@ -1,28 +1,13 @@
-import XCTest
+import Quick
+import Nimble
 @testable import Showroom
 
-class ShowroomTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+class ShowroomTests: QuickSpec {
+    override func spec() {
+        describe("testSpec") {
+            it("test it") {
+                expect(10).to(equal(10))
+            }
         }
     }
-    
 }
