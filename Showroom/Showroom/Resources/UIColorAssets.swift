ccd029b87d77b224deea2e12cbe9f0952b2e786b
Jerzy Kleszcz <jerzy@jerzys-macbook-pro.local>
2016-06-02 14:36:15 +0200

    tab bar icons with insets; tab bar color options

--- Showroom/Showroom/Resources/UIColorAssets.swift
+++ Showroom/Showroom/Resources/UIColorAssets.swift
@@ -1,6 +1,6 @@
 // Generated using SwiftGen, by O.Halligon — https://github.com/AliSoftware/SwiftGen
 
-import UIKit
+import UIKit.UIColor
 
 extension UIColor {
   convenience init(rgbaValue: UInt32) {
@@ -13,32 +13,32 @@ extension UIColor {
   }
 }
 
-extension UIColor {
-  enum Name {
-    /// <span style="display:block;width:3em;height:2em;border:1px solid black;background:#1e1cbf"></span>
-    /// Alpha: 100% <br/> (0x1e1cbfff)
-    case Blue
-    /// <span style="display:block;width:3em;height:2em;border:1px solid black;background:#a4aab3"></span>
-    /// Alpha: 100% <br/> (0xa4aab3ff)
-    case DarkGray
-    /// <span style="display:block;width:3em;height:2em;border:1px solid black;background:#ffffff"></span>
-    /// Alpha: 100% <br/> (0xffffffff)
-    case White
+enum ColorName {
+  /// <span style="display:block;width:3em;height:2em;border:1px solid black;background:#1e1cbf"></span>
+  /// Alpha: 100% <br/> (0x1e1cbfff)
+  case Blue
+  /// <span style="display:block;width:3em;height:2em;border:1px solid black;background:#a4aab3"></span>
+  /// Alpha: 100% <br/> (0xa4aab3ff)
+  case DarkGray
+  /// <span style="display:block;width:3em;height:2em;border:1px solid black;background:#ffffff"></span>
+  /// Alpha: 100% <br/> (0xffffffff)
+  case White
 
-    var rgbaValue: UInt32! {
-      switch self {
-      case .Blue: return 0x1e1cbfff
-      case .DarkGray: return 0xa4aab3ff
-      case .White: return 0xffffffff
-      }
-    }
-
-    var color: UIColor {
-      return UIColor(named: self)
+  var rgbaValue: UInt32! {
+    switch self {
+    case .Blue: return 0x1e1cbfff
+    case .DarkGray: return 0xa4aab3ff
+    case .White: return 0xffffffff
     }
   }
 
-  convenience init(named name: Name) {
+  var color: UIColor {
+    return UIColor(named: self)
+  }
+}
+
+extension UIColor {
+  convenience init(named name: ColorName) {
     self.init(rgbaValue: name.rgbaValue)
   }
 }
--- Showroom/Showroom/Shared/Business/Main/MainTabViewController.swift
+++ Showroom/Showroom/Shared/Business/Main/MainTabViewController.swift
@@ -1,13 +1,25 @@
 import Foundation
 import UIKit
 
+enum TabBarColor {
+    case Blur, White
+    var translucent: Bool { return (self == .Blur) }
+}
+
 class MainTabViewController: UITabBarController {
+    let tabBarItemImageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)  // Center vertically item without title
+    let basketItemImageInsets = UIEdgeInsets(top: 4, left: 0, bottom: -4, right: 0)
+    
+    var tabBarColor: TabBarColor = .Blur
+    
     let resolver: DiResolver
     
     init(resolver: DiResolver) {
         self.resolver = resolver
         
         super.init(nibName: nil, bundle: nil)
+        
+        tabBar.translucent = tabBarColor.translucent
         
         viewControllers = [
             createDashboardViewController(),
@@ -26,31 +38,36 @@ class MainTabViewController: UITabBarController {
     // MARK: - creating child view controllers
     func createDashboardViewController() -> DashboardViewController {
         let viewController = resolver.resolve(DashboardViewController.self)
-        viewController.tabBarItem = UITabBarItem(title: tr(.MainTabDashboard), image: nil, selectedImage: nil)
+        viewController.tabBarItem = UITabBarItem(title: nil, image: UIImage(asset: .Ic_glowna), selectedImage: UIImage(asset: .Ic_glowna_blue))
+        viewController.tabBarItem.imageInsets = tabBarItemImageInsets
         return viewController
     }
     
     func createSearchViewController() -> SearchViewController {
         let viewController = resolver.resolve(SearchViewController.self)
-        viewController.tabBarItem = UITabBarItem(title: tr(.MainTabSearch), image: nil, selectedImage: nil)
+        viewController.tabBarItem = UITabBarItem(title: nil, image: UIImage(asset: .Ic_przegladaj), selectedImage: UIImage(asset: .Ic_przegladaj_blue))
+        viewController.tabBarItem.imageInsets = tabBarItemImageInsets
         return viewController
     }
     
     func createBasketViewController() -> BasketNavigationController {
         let navigationController = resolver.resolve(BasketNavigationController.self)
-        navigationController.tabBarItem = UITabBarItem(title: tr(.MainTabBasket), image: nil, selectedImage: nil)
+        navigationController.tabBarItem = UITabBarItem(title: nil, image: UIImage(asset: .Ic_koszyk), selectedImage: UIImage(asset: .Ic_koszyk_blue))
+        navigationController.tabBarItem.imageInsets = basketItemImageInsets
         return navigationController
     }
     
     func createWishlistViewController() -> WishlistViewController {
         let viewController = resolver.resolve(WishlistViewController.self)
-        viewController.tabBarItem = UITabBarItem(title: tr(.MainTabWishlist), image: nil, selectedImage: nil)
+        viewController.tabBarItem = UITabBarItem(title: nil, image: UIImage(asset: .Ic_ulubione), selectedImage: UIImage(asset: .Ic_ulubione_blue))
+        viewController.tabBarItem.imageInsets = tabBarItemImageInsets
         return viewController
     }
     
     func createSettingsViewController() -> SettingsViewController {
         let viewController = resolver.resolve(SettingsViewController.self)
-        viewController.tabBarItem = UITabBarItem(title: tr(.MainTabSettings), image: nil, selectedImage: nil)
+        viewController.tabBarItem = UITabBarItem(title: nil, image: UIImage(asset: .Ic_profil), selectedImage: UIImage(asset: .Ic_profil_blue))
+        viewController.tabBarItem.imageInsets = tabBarItemImageInsets
         return viewController
     }
 }
