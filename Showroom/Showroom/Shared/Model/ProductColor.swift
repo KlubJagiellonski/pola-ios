5b778d90e14431361de3be70a10ea62aa5b2d89c
Jerzy Kleszcz <jerzy.kleszcz@polidea.com>
2016-06-09 17:33:23 +0200

    connected ProductDetailsSize to ProductSize, connected ProductDetailsColor to ProductColor

--- Showroom/Showroom/Shared/Model/ProductColor.swift
+++ Showroom/Showroom/Shared/Model/ProductColor.swift
@@ -7,6 +7,25 @@ struct ProductColor: CustomStringConvertible {
     let isAvailable: Bool
     
     var description: String { return name }
+    
+    init(productDetailsColor detailsColor: ProductDetailsColor, isAvailable: Bool) {
+        self.id = detailsColor.id
+        self.name = detailsColor.name
+        switch detailsColor.type {
+        case .RGB:
+            self.color = .Color(UIColor(hex: detailsColor.value)!)
+        case .Image:
+            self.color = .ImageUrl(detailsColor.value)
+        }
+        self.isAvailable = isAvailable
+    }
+    
+    init(id: ObjectId, name: String, color: ColorRepresentation, isAvailable: Bool) {
+        self.id = id
+        self.name = name
+        self.color = color
+        self.isAvailable = isAvailable
+    }
 }
 
 typealias Url = String
--- Showroom/Showroom/Shared/Model/ProductSize.swift
+++ Showroom/Showroom/Shared/Model/ProductSize.swift
@@ -6,4 +6,16 @@ struct ProductSize: CustomStringConvertible {
     let isAvailable: Bool
     
     var description: String { return size }
+    
+    init(productDetailsSize: ProductDetailsSize, isAvailable: Bool) {
+        self.id = productDetailsSize.id
+        self.size = productDetailsSize.name
+        self.isAvailable = isAvailable
+    }
+    
+    init(id: ObjectId, size: String, isAvailable: Bool) {
+        self.id = id
+        self.size = size
+        self.isAvailable = isAvailable
+    }
 }
