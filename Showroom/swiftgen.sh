8428270c94af11bb1045680595394e20135c6b9e
Jerzy Kleszcz <jerzy.kleszcz@polidea.com>
2016-11-16 16:04:25 +0100

    added kids colors and running swiftgen.sh on build, ignored swiftgen generated files

--- Showroom/swiftgen.sh
+++ Showroom/swiftgen.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 swiftgen images Showroom/Resources/Assets.xcassets > Showroom/Resources/UIImageAssets.swift
 swiftgen strings Showroom/Resources/pl.lproj/Localizable.strings -p strings-languages.stencil  > Showroom/Resources/L10n.swift
-swiftgen colors Showroom/Resources/Colors.txt > Showroom/Resources/UIColorAssets.swift
+
+if [ "$1" == "ShowroomKids" ]; then
+    swiftgen colors Showroom/Resources/KidsColors.txt > Showroom/Resources/UIColorAssets.swift
+else
+    swiftgen colors Showroom/Resources/Colors.txt > Showroom/Resources/UIColorAssets.swift
+fi
+
