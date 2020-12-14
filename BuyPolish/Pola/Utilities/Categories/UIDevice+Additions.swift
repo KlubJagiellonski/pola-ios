308a3ad93319e108f80c5b669d3e9f203d229c5f
Jakub <34458752+jakub-kalamarz@users.noreply.github.com>
2020-12-14 08:18:01 +0100

    Update UIDevice+Additions.swift (#164)
    * Update UIDevice+Additions.swift
    
    * fix #152

--- BuyPolish/Pola/Utilities/Categories/UIDevice+Additions.swift
+++ BuyPolish/Pola/Utilities/Categories/UIDevice+Additions.swift
@@ -9,7 +9,9 @@ extension UIDevice {
         var deviceInfo = "\n\n-------App & Device info--------\n"
         deviceInfo.append("- system: \(systemName), \(systemVersion)\n")
         let bundle = Bundle.main
-        deviceInfo.append("- app: \(String(describing: bundle.version)), \(String(describing: bundle.shortVersion))\n")
+        if let version = bundle.version, let shortVersion = bundle.shortVersion {
+            deviceInfo.append("- app: \(version), \(shortVersion))\n")
+        }
         deviceInfo.append("-------End-------")
         return deviceInfo
     }
