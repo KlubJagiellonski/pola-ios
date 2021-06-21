0dfbd5cc80f03781e6291633a787a2cb7a0b28a7
Damian <mainczater@gmail.com>
2021-06-21 12:40:11 +0200

    Fixed a scan result background color in a dark mode

--- BuyPolish/Pola/UI/Theme.swift
+++ BuyPolish/Pola/UI/Theme.swift
@@ -11,6 +11,7 @@ final class Theme {
     static let textViewBackgroundColor = R.color.textViewBackgroundColor()!
     static let onlyBlackTextColor = R.color.onlyBlackTextColor()!
     static let backgroundWindowColor = R.color.backgroundWindowColor()!
+    static let clearBackgroundColor = R.color.clearBackgroundColor()!
     static let titleFont = R.font.latoRegular(size: 16)!
     static let captionFont = R.font.latoLight(size: 12)!
     static let normalFont = R.font.latoLight(size: 14)!
