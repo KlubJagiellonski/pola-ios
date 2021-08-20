282ea113fe5afa26d4eaf8726ff6c073a2402d52
Marcin Stepnowski <marcin.stepnowski@gmail.com>
2021-08-20 22:23:11 +0200

    Create deploy workflow

--- BuyPolish/fastlane/Fastfile_deploy.rb
+++ BuyPolish/fastlane/Fastfile_deploy.rb
@@ -6,34 +6,11 @@ platform :ios do
     Options:  
     - version - new version number
     )
-    lane :bump_version do |options|
-      if !options[:version]
-        raise "No version specified!".red
-      end
-      version = options[:version]
-  
-      increment_version_number_in_plist(
-        version_number: version,
-        target: App_sheme
-      )
-      build_number = increment_build_number_in_plist(
-        target: App_sheme
-      )
-
-      snapshot_path = "PolaUITests/Tests/__Snapshots__/InformationPageUITests/testOpenInformationPage.1.png"
-      delete_files(file_pattern:snapshot_path)
-      scan(
-        scheme: App_sheme,
-        testplan: "BumpVersion",
-        fail_build: false
-      )
-
-      plist_path = "Pola/SupportingFiles/Pola-Info.plist"
-      commit_and_push_if_ci(
-        message: "Bump version to #{version} build number to #{build_number}",
-        path: [plist_path, snapshot_path]
-      )
-
+    lane :deploy do |options|
+        # bump_version(
+        #     version: options[:version]
+        # )
+        match
     end
 
   end
