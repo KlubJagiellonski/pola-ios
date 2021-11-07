4f60c2135d64295be9161e554b197ac442ab2884
Jakub <kkalamarz@icloud.com>
2021-11-07 12:36:50 +0100

    Remove unused constant

--- BuyPolish/Pola/AppDelegate.swift
+++ BuyPolish/Pola/AppDelegate.swift
@@ -5,7 +5,6 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    let gcmMessageIDKey = "gcm.message_id"
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         DI.container.resolve(AnalyticsProvider.self)!.configure()
