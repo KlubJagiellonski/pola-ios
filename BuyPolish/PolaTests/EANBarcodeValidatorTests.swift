d6ac1661887fa94a336cfdd52f8dd5234a69d248
Marcin Stepnowski <marcin.stepnowski@gmail.com>
2023-03-14 18:12:45 +0100

    refactor: add DataDrivenTesting to project

--- BuyPolish/PolaTests/EANBarcodeValidatorTests.swift
+++ BuyPolish/PolaTests/EANBarcodeValidatorTests.swift
@@ -1,84 +1,33 @@
+import DataDrivenTesting
 @testable import Pola
 import XCTest
 
 class EANBarcodeValidatorTests: XCTestCase {
-    var sut: EANBarcodeValidator!
+    func testIsValid() {
+        let sut = EANBarcodeValidator()
 
-    override func setUp() {
-        super.setUp()
-        sut = EANBarcodeValidator()
-    }
+        dataTests([
+            TestData((barcode: "40170725", isValid: true)),
+            TestData((barcode: "5902746641132", isValid: true)),
+            TestData((barcode: "5904284420903", isValid: true)),
+            TestData((barcode: "5904277719045", isValid: true)),
+            TestData((barcode: "9788328053045", isValid: true)),
+            TestData((barcode: "978832805304", isValid: false)),
+            TestData((barcode: "97883280530", isValid: false)),
+            TestData((barcode: "978832805", isValid: false)),
+            TestData((barcode: "97883280", isValid: false)),
+            TestData((barcode: "9788328", isValid: false)),
+            TestData((barcode: "978832", isValid: false)),
+            TestData((barcode: "97883", isValid: false)),
+            TestData((barcode: "9788", isValid: false)),
+            TestData((barcode: "978", isValid: false)),
+            TestData((barcode: "97", isValid: false)),
+            TestData((barcode: "7", isValid: false)),
 
-    override func tearDown() {
-        sut = nil
-        super.tearDown()
-    }
+        ]) { testData, _ in
+            let result = sut.isValid(barcode: testData.data.barcode)
 
-    func testReturnTrueWhen40170725() {
-        XCTAssertTrue(sut.isValid(barcode: "40170725"))
-    }
-
-    func testReturnTrueWhen5902746641132() {
-        XCTAssertTrue(sut.isValid(barcode: "5902746641132"))
-    }
-
-    func testReturnTrueWhen5904284420903() {
-        XCTAssertTrue(sut.isValid(barcode: "5904284420903"))
-    }
-
-    func testReturnTrueWhen5904277719045() {
-        XCTAssertTrue(sut.isValid(barcode: "5904277719045"))
-    }
-
-    func testReturnTrueWhen9788328053045() {
-        XCTAssertTrue(sut.isValid(barcode: "9788328053045"))
-    }
-
-    func testReturnFalseWhen978832805304() {
-        XCTAssertFalse(sut.isValid(barcode: "978832805304"))
-    }
-
-    func testReturnFalseWhen97883280530() {
-        XCTAssertFalse(sut.isValid(barcode: "97883280530"))
-    }
-
-    func testReturnFalseWhen9788328053() {
-        XCTAssertFalse(sut.isValid(barcode: "9788328053"))
-    }
-
-    func testReturnFalseWhen978832805() {
-        XCTAssertFalse(sut.isValid(barcode: "978832805"))
-    }
-
-    func testReturnFalseWhen97883280() {
-        XCTAssertFalse(sut.isValid(barcode: "97883280"))
-    }
-
-    func testReturnFalseWhen9788328() {
-        XCTAssertFalse(sut.isValid(barcode: "9788328"))
-    }
-
-    func testReturnFalseWhen978832() {
-        XCTAssertFalse(sut.isValid(barcode: "978832"))
-    }
-
-    func testReturnFalseWhen97883() {
-        XCTAssertFalse(sut.isValid(barcode: "97883"))
-    }
-
-    func testReturnFalseWhen9788() {
-        XCTAssertFalse(sut.isValid(barcode: "9788"))
-    }
-
-    func testReturnFalseWhen978() {
-        XCTAssertFalse(sut.isValid(barcode: "978"))
-    }
-
-    func testReturnFalseWhen97() {
-        XCTAssertFalse(sut.isValid(barcode: "97"))
-    }
-
-    func testReturnFalseWhen9() {
-        XCTAssertFalse(sut.isValid(barcode: "9"))
+            XCTAssertEqual(result, testData.data.isValid, file: testData.file, line: testData.line)
+        }
     }
 }
